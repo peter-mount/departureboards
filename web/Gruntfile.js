0aff4071c6fbd3645a1f3d6467595bbecb3f1bfd
Peter Mount <peter@retep.org>
2016-10-12 21:00:06 +0100

    More grunt work reducing the number of files needed to be transfered

--- web/Gruntfile.js
+++ web/Gruntfile.js
@@ -14,10 +14,22 @@ module.exports = function (grunt) {
             target: {
                 files: {
                     "public_html/departure-min.css": [
-                        "public_html/ldb.css",
-                        "public_html/mobile.css"
+                        "public_html/css/bootstrap.min.css",
+                        "public_html/css/ldb.css",
+                        "public_html/css/mobile.css"
                     ]
                 }
+            }
+        },
+        concat:{
+            dist: {
+                src: [
+                    "public_html/js/jquery-3.1.1.min.js",
+                    "public_html/js/bootstrap.min.js",
+                    "public_html/js/typeahead.min.js",
+                    "public_html/js/departure.js"
+                ],
+                dest: "public_html/departure.js"
             }
         },
         uglify: {
@@ -28,8 +40,9 @@ module.exports = function (grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('build', ['cssmin','uglify']);
+    grunt.registerTask('build', ['cssmin','concat','uglify']);
 };
---
+++ web/public_html/js/departure.js
@@ -0,0 +1,190 @@
+var UI = (function () {
+    function UI() {
+
+    }
+    UI.urlChange = function (l) {
+        // Comment out when running within NetBeans
+        //history.pushState(history.state, null, l);
+    };
+
+    var panes = ['#searchPane', '#boardPane'];
+    UI.show = function (id) {
+        $.each(panes, function (i, p) {
+            $(p).css({display: p === id ? "block" : "none"});
+        });
+    };
+
+    UI.showAbout = function () {
+        $("#about").modal();
+    };
+
+    UI.showContact = function () {
+        $("#contactus").modal();
+    };
+
+    // ==== Station search ====
+    // Last received station results
+    var stationResults = {};
+    // Show the station page
+    UI.showSearch = function () {
+        UI.urlChange('/');
+        UI.show("#searchPane");
+        setTimeout(function () {
+            $('#stations').val('').focus();
+        }, 250);
+    };
+
+    // Parse received json into stationResults & typeahead
+    UI.stationSearch = function (parsedResponse) {
+        stationResults = {};
+        var ary = $.map(parsedResponse, function (loc) {
+            loc.text = loc.name + " [" + loc.crs + "]";
+            stationResults[loc.text] = loc;
+            return loc.text;
+        });
+        return ary;
+    };
+
+    // Select station
+    UI.stationSearchOn = function (e) {
+        var t = $('#stations').val();
+        var loc = stationResults[t];
+        if (loc && loc.crs && loc.crs !== '')
+            UI.showCRS(loc.crs);
+    };
+
+    // ===== Station page =====
+    var stationCrs;
+    UI.showCRS = function (crs) {
+        stationCrs = crs.toUpperCase();
+        UI.urlChange('/' + stationCrs);
+        // Clear any existing data
+        $('#boardName').empty();
+        $('#boardTable').empty();
+        // Show the boards and refresh
+        UI.show("#boardPane");
+        UI.refreshBoard();
+    };
+    UI.refreshBoard = function () {
+        if (!stationCrs)
+            UI.showSearch();
+        else
+            $.ajax({
+                url: "//api.area51.onl/darwin/departures/" + stationCrs,
+                dataType: 'json',
+                cache: false,
+                success: showCrsBoard
+            });
+    };
+
+    var a = function () {
+        return $('<a></a>');
+    };
+    var div = function () {
+        return $('<div></div>');
+    };
+    var span = function () {
+        return $('<span></span>');
+    };
+    var parseTime = function (d, t) {
+        //var a=t.split(':');
+        //return (+a[0]*60)+(+a[1]);
+        return Date.parse(d + "T" + t);
+    };
+    var parseLocTime = function (a) {
+        return parseTime(a.ssd, a.loc.ptd ? a.loc.ptd : a.loc.pta ? a.loc.pta : a.loc.wtd ? a.loc.wtd : a.loc.wta);
+    };
+    var showCrsBoard = function (data) {
+        $('#boardName').empty().append(data.station.name);
+        var tab = $('#boardTable');
+        tab.empty();
+
+        if (!data.departures || data.departures.length === 0) {
+            // Show no data
+            div().appendTo(tab).append("No data available");
+        } else {
+            var altrow = 0;
+
+            // sort
+            data.departures.sort(function (a, b) {
+                return parseLocTime(a) - parseLocTime(b);
+            });
+
+            // Don't show too far in the future (we could see tomorrows data in here)
+            var now = Date.now() + 86400000 - 3600000;
+            $.each(data.departures, function (i, v) {
+                // Don't go too far ahead
+                if (parseLocTime(v) >= now)
+                    return;
+
+                // Hide non-public trains unless we want them
+                if (!v.loc.pta && !v.loc.ptd)
+                    return;
+
+                var row = div().appendTo(tab);
+                altrow = 1 - altrow;
+                if (altrow)
+                    row.addClass("altrow");
+
+                var terminates = v.dest.tpl.tiploc === v.loc.tpl.tiploc;
+
+                var d = div().addClass("ldb-enttop").appendTo(row)
+                        .append(div().addClass("ldbCol").addClass("ldbForecast"))
+                        .append(div().addClass("ldbCol").addClass("ldbSched").append(v.loc.ptd ? v.loc.ptd : v.loc.pta))
+                        .append(div().addClass("ldbCol").addClass("ldbPlat").append(v.loc.plat ? v.loc.plat : ""));
+                var cont = div().addClass("ldbCont").appendTo(d)
+                        .append(a().append(terminates ? "Terminates here" : v.dest.tpl.name));
+                if (v.via)
+                    cont.append(span().addClass("ldbVia").append(" via " + v.via));
+
+                if (!terminates) {
+                    d = div().addClass("ldb-entbot").appendTo(row)
+                            .append(span().addClass("ldbHeader").addClass("callList").append("Calling at:"));
+                    // todo add cp list to api output, for now just the dest
+                    d.append(span().addClass("callList").append(" ")
+                            .append(a().append(a().append(v.dest.tpl.name).click(function (e) {
+                                UI.showCRS(v.dest.tpl.crs);
+                            })))
+                            .append(" (").append((v.dest.pta ? v.dest.pta : v.dest.wta).substr(0, 5)).append(")"));
+                }
+
+                // Operator and last report lines
+                d = div().addClass("ldb-entbot").appendTo(row);
+                // todo add full name to toc
+                if (v.toc)
+                    d.append(span().append(v.toc).append("&nbsp;service. "));
+                // todo last report
+                //d.append(span().addClass("ldbHeader").append("Last report: "))
+                //        .append(span().addClass("ldbDest").append(v.loc.tpl).append(" 00:00 "));
+            });
+        }
+    };
+
+    return UI;
+})();
+
+$(document).ready(function () {
+    console.log("init");
+    $('#stations').typeahead({
+        name: "search",
+        remote: {
+            url: '//api.area51.onl/darwin/search?term=%QUERY',
+            dataType: 'json',
+            cache: false,
+            filter: UI.stationSearch
+        }
+    }).on("typeahead:selected", UI.stationSearchOn);
+
+    // Get the page url, remove any / or /index.html
+    var l = location.pathname;
+    while (l.endsWith("/index.html"))
+        l = l.substr(0, l.length - "/index.html".length);
+    while (l.endsWith("/"))
+        l = l.substr(0, l.length - 1);
+
+    // Show station page based on crs code in url, otherwise the search page
+    if (l.match("/[a-zA-Z]{3}"))
+        UI.showCRS(l.substr(1));
+    else
+        UI.showSearch();
+});
