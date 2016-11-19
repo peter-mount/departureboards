ec0bd31ace4c77cbe4e2054de4199fec19826c28
Peter Mount <peter@retep.org>
2016-11-19 09:35:16 +0000

    Start migrating page to new api

--- web/public_html/js/departure.js
+++ web/public_html/js/departure.js
@@ -5,7 +5,8 @@ var UI = (function () {
     }
     UI.urlChange = function (l) {
         // Comment out when running within NetBeans
-        history.pushState(history.state, null, l);
+        if (window.location.hostname !== 'localhost')
+            history.pushState(history.state, null, l);
     };
 
     var panes = ['#searchPane', '#boardPane'];
@@ -39,23 +40,14 @@ var UI = (function () {
 
     // Parse received json into stationResults & typeahead
     UI.stationSearch = function (parsedResponse) {
-        var t = $('#stations').val().toUpperCase();
-        parsedResponse.sort(t.length === 3 ? function (a, b) {
-            return a.crs === t ? -2 : b.crs === t ? 2 : a.name < b.name ? -1 : 1;
-        } : function (a, b) {
-            return a.name < b.name ? -1 : 1;
-        });
-        stationResults = {};
-        var ary = [];
-        $.each(parsedResponse, function (i, loc) {
-            if (loc.tiploc !== loc.name) {
-                loc.text = loc.name + " [" + loc.crs + "]";
-                stationResults[loc.text] = loc;
-                ary.push(loc.text);
-            }
-        });
-        console.log(stationResults);
-        return ary;
+        stationResults = parsedResponse.reduce(function (a, b) {
+            a[b.label] = b;
+            return a;
+        }, {});
+        return parsedResponse.reduce(function (a, b) {
+            a.push(b.label);
+            return a;
+        }, []);
     };
 
     // Select station
@@ -63,8 +55,8 @@ var UI = (function () {
         var t = $('#stations').val();
         var loc = stationResults[t];
         $('#stations').typeahead('setQuery', '');
-        if (loc && loc.crs && loc.crs !== '')
-            UI.showCRS(loc.crs);
+        if (loc)
+            UI.showCRS(loc.code);
     };
 
     // ===== Station page =====
@@ -72,8 +64,8 @@ var UI = (function () {
     UI.showCRS = function (crs) {
         stationCrs = crs.toUpperCase();
         UI.urlChange('/' + stationCrs);
-        // Clear any existing data
-        $('#boardName').empty();
+        // Clear any existing data nbsp to stop annoying pull up of headers
+        $('#boardName').empty().append('&nbsp;');
         $('#boardTable').empty();
         // Show the boards and refresh
         UI.show("#boardPane");
@@ -82,13 +74,16 @@ var UI = (function () {
     UI.refreshBoard = function () {
         if (!stationCrs)
             UI.showSearch();
-        else
+        else {
+            var url = "//api.area51.onl/rail/1/station/" + stationCrs + "/boards";
+            console.log("refresh: " + stationCrs + " = " + url);
             $.ajax({
-                url: "//api.area51.onl/darwin/departures/" + stationCrs,
+                url: url,
                 dataType: 'json',
                 cache: false,
                 success: showCrsBoard
             });
+        }
     };
 
     var a = function () {
@@ -106,7 +101,23 @@ var UI = (function () {
     var parseLocTime = function (a) {
         return parseTime(a.ssd, a.loc.ptd ? a.loc.ptd : a.loc.pta ? a.loc.pta : a.loc.wtd ? a.loc.wtd : a.loc.wta);
     };
+    // glyphicons 
+    var icons ={
+        ALERT:'glyphicon-alert',
+        INFO:'glyphicon-info-sign',
+        WARNING:'glyphicon-warning-sign',
+        NOTICE:'glyphicon-exclamation-sign',
+        QUESTION:'glyphicon-info-sign',
+        OK:'glyphicon-ok-sign',
+        GLOBAL:'glyphicon-globe',
+        HEART:'glyphicon-heart-empty',
+        PHONE:'glyphicon-earphone',
+        FOOD:'glyphicon-cutlery',
+        PLUS:'glyphicon-plus-sign',
+        MINUS:'glyphicon-minus-sign'
+    };
     var showCrsBoard = function (data) {
+        console.log(JSON.stringify(data));
         $('#boardName').empty().append(data.station.name);
         var tab = $('#boardTable');
         tab.empty();
@@ -117,61 +128,97 @@ var UI = (function () {
         } else {
             var altrow = 0;
 
+            if (data.messages)
+                data.messages.forEach(function (m) {
+                    var row = div().appendTo(tab);
+                    altrow = 1 - altrow;
+                    if (altrow)
+                        row.addClass("altrow");
+
+                    d = div().addClass("ldb-enttop").appendTo(row)
+                    .append('<div class="glyphicon '+(m.icon&&icons[m.icon]?icons[m.icon]:icons.INFO)+'" aria-hidden="true" style="float:left;font-size: 200%;font-weight:normal;margin: 0.2em;"></div>')
+                    .append(m.message);
+                    d = div().addClass("ldb-entbot").appendTo(row).append('&nbsp;');
+                });
+
             // sort
-            data.departures.sort(function (a, b) {
-                return parseLocTime(a) - parseLocTime(b);
-            });
+            //data.departures.sort(function (a, b) {                return parseLocTime(a) - parseLocTime(b);            });
 
             // Don't show too far in the future (we could see tomorrows data in here)
             var now = Date.now() + 86400000 - 3600000;
-            $.each(data.departures, function (i, v) {
+            data.departures.forEach(function (v) {
                 // Don't go too far ahead
-                if (parseLocTime(v) >= now)
-                    return;
-
-                // Hide non-public trains unless we want them
-                if (!v.loc.pta && !v.loc.ptd)
-                    return;
+                //if (parseLocTime(v) >= now)                    return;
 
                 // Optional hide terminating trains
-                var terminates = v.dest.tpl.tiploc === v.loc.tpl.tiploc;
-                if (terminates)
-                    return;
+                //if (v.status.terminatesHere)                    return;
 
                 var row = div().appendTo(tab);
                 altrow = 1 - altrow;
                 if (altrow)
                     row.addClass("altrow");
 
-                var d = div().addClass("ldb-enttop").appendTo(row)
-                        .append(div().addClass("ldbCol").addClass("ldbForecast"))
-                        .append(div().addClass("ldbCol").addClass("ldbSched").append(v.loc.ptd ? v.loc.ptd : v.loc.pta))
-                        .append(div().addClass("ldbCol").addClass("ldbPlat").append(v.loc.plat ? v.loc.plat : ""));
-                var cont = div().addClass("ldbCont").appendTo(d)
-                        .append(a().append(terminates ? "Terminates here" : v.dest.tpl.name));
-                if (v.via)
-                    cont.append(span().addClass("ldbVia").append(" via " + v.via));
+                var d = div().addClass("ldb-enttop").appendTo(row);
+                var fd = div().appendTo(d).addClass("ldbCol").addClass("ldbForecast");
+                if (v.status.cancelled)
+                    fd.append('Cancelled');
+                else if (v.status.arrived)
+                    fd.append('Arrived');
+                else if (v.status.delayed)
+                    fd.append('Delayed');
+                else if (v.status.time === v.forecast.ptd || v.status.time === v.forecast.pta)
+                    fd.append('On&nbsp;Time');
+                else
+                    fd.append(v.status.time);
 
-                if (!terminates) {
-                    d = div().addClass("ldb-entbot").appendTo(row)
-                            .append(span().addClass("ldbHeader").addClass("callList").append("Calling at:"));
-                    // todo add cp list to api output, for now just the dest
-                    d.append(span().addClass("callList").append(" ")
-                            .append(a().append(a().append(v.dest.tpl.name).click(function (e) {
-                                UI.showCRS(v.dest.tpl.crs);
-                            })))
-                            .append(" (").append((v.dest.pta ? v.dest.pta : v.dest.wta).substr(0, 5)).append(")"));
+                // make easier
+                d.append(div().addClass("ldbCol").addClass("ldbSched").append(v.forecast.ptd ? v.forecast.ptd : v.forecast.pta))
+                        .append(div().addClass("ldbCol").addClass("ldbPlat").append(v.status.platform));
+                var cont = div().addClass("ldbCont").appendTo(d)
+                        .append(a().append(v.status.terminatesHere ? "Terminates here" : v.train.dest));
+                if (v.status.via)
+                    cont.append(span().addClass("ldbVia").append(" via " + v.status.via));
+
+                if (v.status.reason)
+                    div().appendTo(row).addClass("ldbCont").append(v.status.reason);
+
+                if (!v.status.terminatesHere && !v.status.cancelled && v.calling && v.calling.length > 0) {
+                    d = div().addClass("ldb-entbot").appendTo(row).append("Calling at:");
+                    v.calling.forEach(function (cp) {
+                        d.append(span().addClass("callList").append(" ")
+                                .append(a().append(a().append(cp.name).click(function (e) {
+                                    UI.showCRS(cp.crs);
+                                })))
+                                .append("&nbsp;(").append(cp.time).append(")"));
+                    });
                 }
 
                 // Operator and last report lines
                 d = div().addClass("ldb-entbot").appendTo(row);
                 // todo add full name to toc
-                if (v.toc)
-                    d.append(span().append(v.toc).append("&nbsp;service. "));
-                // todo last report
-                //d.append(span().addClass("ldbHeader").append("Last report: "))
-                //        .append(span().addClass("ldbDest").append(v.loc.tpl).append(" 00:00 "));
+                if (v.train.toc)
+                    span().appendTo(d).append(v.train.toc).append("&nbsp;service. ");
+
+                if (v.status.length)
+                    d.append(span().addClass("ldbHeader").append(" Formed&nbsp;of&nbsp;"))
+                            .append(v.status.length)
+                            .append(span().addClass("ldbDest").append("coaches"));
+
+                if (v.status.delay)
+                    d.append(span().addClass("ldbHeader").append("Delayed&nbsp;by&nbsp;"))
+                            .append(v.status.delay)
+                            .append(span().addClass("ldbHeader").append(Math.abs(v.status.delay) == 1 ? " minute" : " minutes"));
+
+                d = div().addClass("ldb-entbot").appendTo(row);
+                if (v.status.lastReport)
+                    d.append("Last report: ")
+                            .append(span().addClass("ldbDest").append(v.status.lastReport.name))
+                            .append("&nbsp;(").append(v.status.lastReport.time).append(') ');
+
             });
+
+            // FIXME adds padding to bottom of page, scrolling hides otherwise with the footer
+            div().appendTo(tab).append('&nbsp;');
         }
     };
 
@@ -189,9 +236,9 @@ $(document).ready(function () {
         limit: 10,
         name: "search",
         remote: {
-            url: '//api.area51.onl/darwin/search?term=%QUERY',
+            url: '//api.area51.onl/rail/1/search?q=%QUERY',
             dataType: 'json',
-            cache: false,
+            cache: true,
             filter: UI.stationSearch
         }
     }).on("typeahead:selected", UI.stationSearchOn);
@@ -205,9 +252,9 @@ $(document).ready(function () {
 
     // Show station page based on crs code in url, otherwise the search page
     // /mldb/ is to support old url's from the old app
-    if (1)
-        UI.showCRS('VIC');
-    else
+//    if (1)
+//        UI.showCRS('LBG');
+//    else
     if (l.match("/mldb/[a-zA-Z]{3}"))
         UI.showCRS(l.substr(6));
     else if (l.match("/[a-zA-Z]{3}"))
