a71d81f3b9ea3b27679e941cdd384567d2180a0a
Peter Mount <peter@retep.org>
2018-03-13 15:10:12 +0000

    Homepage

---
+++ src/home/HomePage.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+
+import { withRouter } from 'react-router';
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
+import EUCookie from 'area51-eucookie';
+
+class HomePage extends Component {
+
+  constructor( props ) {
+    super( props );
+    this.state = { options: [] };
+  }
+
+  // Perform the query
+  search(query, t) {
+    fetch('https://ref.a.a51.li/search/' + query)
+      .then(resp => resp.json())
+      .then(json => t.setState({
+        options: json
+      }))
+      .catch((e)=>{
+        // Retry after 1 second
+        setTimeout(()=>t.search(query,t),1000);
+      });
+  }
+
+  // Handle the selection. Only pass 1 entry to the app
+  select(selection, t) {
+    if (selection && selection.length === 1) {
+      console.log( selection[0]);
+      //t.props.app.boards(selection[0].code, t.props.app);
+    }
+  }
+
+  render() {
+    return (<div>
+      <EUCookie />
+      <div className="App-header">
+        <h1>Realtime UK Departure Boards</h1>
+
+      </div>
+      <div className="App-intro">
+        <div className="station-form">
+          <div>
+            <div id="stationlabel" htmlFor="stations">
+              Please enter the station you want to view below:
+            </div>
+            <AsyncTypeahead
+              id="stations"
+              useCache={false}
+              minLength={3}
+              onSearch={query => this.search(query, this)}
+              onChange={selection => this.select(selection, this)}
+              options={this.state.options}
+              placeholder="Type a station name, postcode or CRS code"
+              ref={(input) => this.focus = input}
+              filterBy={(o, t) => true}
+            />
+          </div>
+        </div>
+        <p>
+        </p>
+        <p>
+          To use simply enter the UK Rail station name in the box above and you will be shown the current boards for that station.
+        </p>
+        <p>
+          Alternatively you can enter a UK PostCode and you will be shown the nearest stations to that address.
+        </p>
+        <p>
+          By clicking the destination name on those boards you will be able to view the current progress of that service.
+        </p>
+      </div>
+    </div>);
+  }
+
+}
+
+export default withRouter( HomePage );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import { render } from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import Boards from "./boards/Boards";
+import HomePage from "./home/HomePage";
 
 render(
   <BrowserRouter>
     <div>
+      <Route
+        component={HomePage}
+        exact
+        path='/'
+      />
       <Route
         component={Boards}
         exact
