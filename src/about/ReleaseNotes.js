0d83559e520d9c0ffc0c4e05eb96971c8696c0ee
peter <peter@retep.org>
2019-09-16 19:50:13 +0100

    Move release notes to release notes page

--- src/about/ReleaseNotes.js
+++ src/about/ReleaseNotes.js
@@ -3,10 +3,12 @@ import React, {Component} from 'react';
 import {withRouter} from 'react-router';
 import EUCookie from 'area51-eucookie';
 import Navigation from '../Navigation';
+import {NavItem} from "react-bootstrap";
 
 class ReleaseNotes extends Component {
 
     render() {
+        const t = this;
         return (<div>
             <Navigation page="releaseNotes"/>
             <EUCookie/>
@@ -20,6 +22,9 @@ class ReleaseNotes extends Component {
                     <li>Ability to limit the number of services returned rather than all services in the next hour.</li>
                     <li>Terminating services are not returned unless you request it in configuration.</li>
                     <li>Ability to use our UAT environment instead of the Live one.</li>
+                    <li>Release notes are now available via the <a
+                        onClick={() => t.props.history.push("/releaseNotes")}>Whats New</a> page.
+                    </li>
                 </ul>
             </div>
         </div>);
